710eb437ebb2bb67e6a3f5e1c65f956487e8ce62
mainaak <mainaakch@gmail.com>
2023-12-14 22:34:08 +0530

    fix fuzzy, update requirements

--- flair_token.py
+++ flair_token.py
@@ -1,5 +1,3 @@
-import numpy as np
-import pandas as pd
 from flair.data import Sentence
 from flair.models import SequenceTagger
 import torch, flair
@@ -18,31 +16,9 @@ def tokenise_loc(sentence_rec):
     query_string = sentence_rec
     sentence = Sentence(query_string)
     classifier.predict(sentence)
-    print(sentence)
-    # ner_dict=sentence.to_dict(tag_type='ner')
-    # ner_df = pd.DataFrame(ner_dict)
-    # print(ner_df)
+    # print(sentence)
     df=[]
     for label in sentence.get_labels():
         df.append((label.data_point.text, label.value))
     
     return df
-
-# async def get_possible_loc_tokens(sentence_rec):
-#     query_string = sentence_rec
-#     sentence = Sentence(query_string)
-#     loss = tagger.predict((sentence), return_loss=True)
-
-#     doc = nlp(sentence_rec)
-
-#     df=[]
-#     for token in doc:
-#         # print(token.text, token.pos_)
-#         if len(token.text)>=4 and (token.pos_=="PROPN" or token.pos_=="NOUN" or token.pos_=="X"):
-#             df.append((token.text, token.pos_))
-
-#     # for label in sentence.get_labels():
-#     #     if len(label.data_point.text)>=3 and (label.value=="PROPN" or label.value=="X"):
-#     #         df.append((label.data_point.text, label.value))
-
-#     return df
